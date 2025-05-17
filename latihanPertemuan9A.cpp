21390d0a872539260d120c9f811b501a5f0c4969
Fanshi13 <aquillafazanaufan@gmail.com>
2025-05-17 16:17:40 +0700

    commit 3

--- latihanPertemuan9A.cpp
+++ latihanPertemuan9A.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 class Mahasiswa;
 
-class Dosen private : string nama;
-string NIDN;
-string pangkat;
-float gaji;
+class Dosen
+{
+private:
+    string nama;
+    string NIDN;
+    string pangkat;
+    float gaji;
+
+public:
+    Dosen(string nama, string nidn, string pangkat, float gaji)
+        : nama(nama), NIDN(nidn), pangkat(pangkat), gaji(gaji) {}
+
+    friend class Staff;                    // agar Staff bisa mengubah pangkat
+    friend float aksesGajiDosen(Dosen *d); // hanya akses gaji, tidak semua data
+    void beriNilai(Mahasiswa *m, float nilai);
+};
